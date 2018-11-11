9893ea2da69fa754c880bb7e643f0d05d48c64a5
Ange Santos <angel_santos@outlook.com>
2018-11-11 01:07:09 -0600

    init migrations fo tokens

--- betterfuture/tokens/admin.py
+++ betterfuture/tokens/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from .models import Token, TokenTransactions, Transaction
+
 # Register your models here.
+class TokenAdmin(admin.ModelAdmin):
+    list_display = ('eth_address', 'eth_per_token')
+admin.site.register(Token, TokenAdmin)
+
+class TokenTransactionsAdmin(admin.ModelAdmin):
+    list_display = ('address_from', 'address_to', 'contract', 'gas', 'eth', 'transaction')
+admin.site.register(TokenTransactions, TokenTransactionsAdmin)
+
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'user_footprint', 'tokens_value')
+admin.site.register(Transaction, TransactionAdmin)
--- betterfuture/tokens/models.py
+++ betterfuture/tokens/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
+from shortuuidfield import ShortUUIDField
+
+from betterfuture.users.models import User, UserFootPrint
 # Create your models here.
+
+class Token(models.Model):
+    eth_address = models.CharField('Core ETH address', max_length=42)
+    eth_per_token = models.DecimalField('ETH per Token', max_digits=19, decimal_places=10)
+
+    def __str__(self):
+        return self.eth_address
+
+class TokenTransactions(models.Model):
+    uuid = ShortUUIDField(primary_key=True)
+    address_from = models.CharField('ETH address FROM', max_length=42, blank=True)
+    address_to = models.CharField('ETH address TO', max_length=42, blank=True)
+    contract = models.CharField('Contract address', max_length=42, blank=True)
+    gas = models.DecimalField('Gas', max_digits=19, decimal_places=10)
+    eth = models.DecimalField('Sent ETH', max_digits=19, decimal_places=10)
+    transaction = models.ForeignKey('Transaction', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return 'Blockchain transaction for {}'.format(self.transaction)
+
+class Transaction(models.Model):
+    uuid = ShortUUIDField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user_footprint = models.ForeignKey(UserFootPrint, on_delete=models.CASCADE)
+    tokens_value = models.DecimalField('Tokens', max_digits=19, decimal_places=10) 
+
+    def __str__(self):
+        return 'Internal transaction for User: {}, Footprint: {}'.format(self.user, self.user_footprint.footprint)
